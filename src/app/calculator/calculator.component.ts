ef97a43f0cd49979e076e3da980e63de8014c457
Vidalatan <vidalatan@gmail.com>
2022-06-16 00:43:34 -0500

    Associated functions to buttons

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -19,22 +19,81 @@ export class CalculatorComponent implements OnInit {
 
   // Used to show the current display
   public display:string = '';
-
-
+  
+  
   // Each button symbol or number
-  public buttons:string[] = 
+  public buttons: any[][] = 
   [
-    '%',  'CE', 'C', '«',
-    '1/x','x^2','√a','÷',
-    '7',  '8',  '9', '×',
-    '4',  '5',  '6', '-',
-    '1',  '2',  '3', '+',
-    '±',  '0',  '.', '='
+    ['%', this.doPercent], ['CE', this.doClearEntry],
+    [ 'C', this.doClear], ['«', this.doBackSpace],
+    ['1/x', this.doInverse], ['x^2', this.doSquare],   
+    ['√a', this.doSqrt], ['÷', this.doDivision],
+    ['7'],  ['8'],  ['9'], ['×', this.doMultiply],
+    ['4'],  ['5'],  ['6'], ['-', this.doSubtraction],
+    ['1'],  ['2'],  ['3'], ['+', this.doAddition],
+    ['±', this.doSwitchSign],  ['0'],  ['.'], ['=', this.doSolve]
   ]
-
+  
   // Check if button char is just a number
   isNumber(char:string){
     return char.match(/^[0-9]$/)
   }
+  
+  doPercent(){
+   return 
+  }
 
+  doClearEntry(){
+
+  }
+
+  doClear(){
+
+  }
+
+  doBackSpace(){
+
+  }
+
+  doInverse(){
+
+  }
+
+  doSquare(){
+
+  }
+
+  doSqrt(){
+
+  }
+
+  doDivision(){
+
+  }
+
+  doMultiply(){
+
+  }
+
+  doSubtraction(){
+
+  }
+
+  doAddition(){
+
+  }
+
+  doSwitchSign(){
+
+  }
+
+  doSolve(){
+
+  }
+
+
+  test(event:object){
+    console.log(event);
+    
+  }
 }
